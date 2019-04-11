e4e8a9e9da666624c1bd82009c8b4a0cc665ffd8
LuYing <597729230@qq.com>
2019-04-11 15:45:17 +0800

    003

--- xiaoming/src/main/java/HelloGit.java
+++ xiaoming/src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第一次在IDEA上使用Git");
         System.out.println("为什么提交不上去呢");
+        System.out.println("继续测试提交");
     }
 }
